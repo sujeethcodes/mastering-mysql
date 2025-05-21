a931c3413137448d6fbcf5332fb27e509e1a9fca
sujeethcodes <sujeethcodes@gmail.com>
2025-05-21 12:22:39 +0530

    insert-query

---
+++ crud_opreations/insert.sql
@@ -0,0 +1,8 @@
+-- INSERT VALUES IN employees TABLE 
+
+INSERT INTO employees (
+  name, age, email, role, employee_id, status, joining_date
+)
+VALUES (
+  'sujeeth', 26, 'sujeethcodes@gmail.com', 'software developer', 'qw12eqe', 1, '2025-01-01'
+);
